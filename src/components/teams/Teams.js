05581e61fa07959b81ba34db0b5f2092c95e6aef
AnnisaDmynt221 <annisa_damayanti@students.amikom.ac.id>
2023-06-27 14:19:22 +0700

    punya nisa

--- src/components/teams/Teams.js
+++ src/components/teams/Teams.js
@@ -3,16 +3,16 @@ import TeamsCard from './TeamsCard'
 
 export default function Teams() {
   return (
-    <div className='min-h-screen w-full bg-white flex items-center flex-col'>
-        <div className='flex flex-col items-center'>
-            <h1 className='mb-20 mt-20 font-extrabold text-3xl'>Tim Kita</h1>
+    <div className='min-h-screen w-full bg-[#0C4C4C] flex items-center flex-col'>
+        <div className='flex flex-col items-center my-10'>
+            <h1 className='font-extrabold text-3xl text-[#E9F5F5]'>Tim Kita</h1>
         </div>
-        <div className='flex flex-wrap gap-36 max-w-[70vw] justify-center items-center'>
-            <TeamsCard name={"Annisa Damayanti"} nim={"21.11.4260"}/>
+        <div className='grid grid-cols-2 flex-wrap gap-7 max-w-[50vw] justify-center items-center'>
             <TeamsCard name={"Aryo Bimo Wicaksono"} nim={"21.11.4311"}/>
-            <TeamsCard name={"Mauhiba Salmaa Ghaisani"} nim={"21.11.4264"}/>
-            <TeamsCard name={"Ameilia Nurcahyandari Sutomo"} nim={"21.11.4314"}/>
+            <TeamsCard name={"Ameilia Nurcahyandari S."} nim={"21.11.4314"}/>
+            <TeamsCard name={"Annisa Damayanti"} nim={"21.11.4260"}/>
+            <TeamsCard name={"Mauhiba Salmaa G."} nim={"21.11.4264"}/>
         </div>
-    </div>
-  )
+    </div>
+  )
 }
--- src/components/teams/TeamsCard.js
+++ src/components/teams/TeamsCard.js
@@ -2,9 +2,10 @@ import Image from 'next/image'
 import React from 'react'
 import pp from '/public/assets/pp.jpg'
 
-export default function TeamsCard({name, nim}) {
+export default function TeamsCard ({name, nim}) {
   return (
     <div className="card card-side bg-base-100 shadow-xl h-52 items-center">
+        <div className>
         <div className="avatar p-5">
             <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
                 <Image src={pp} width={0} height={0} alt="img"/>
@@ -13,8 +14,10 @@ export default function TeamsCard({name, nim}) {
         <div className="card-body">
             <h2 className="card-title">{name}</h2>
             <h2 className="card-title">{nim}</h2>
-            <p>Click the button to watch on Jetflix app.</p>
+            <p></p>
         </div>
     </div>
-  )
+    </div>
+    
+  )
 }
