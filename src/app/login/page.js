940c913bfe5411240c3b79b2f894663d73e8fd72
Ameilianurcahya <ameiliajogja3005@gmail.com>
2023-06-28 19:50:42 +0700

    update amel -2

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -3,19 +3,19 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div className='bg-white w-[100vw] h-[100vh] flex justify-center items-center'>
-      <div className="card bg-[#E3F4F4] shadow-2xl shadow-[#E3F4F4] w-[420px] h-[495px] rounded-[53px]">
+    <div className='bg-white min-h-screen flex justify-center items-center'>
+      <div className="card bg-[#E3F4F4] shadow-2xl shadow-[#E3F4F4] w-[90%] max-w-[420px] h-auto rounded-[53px]">
         <div className="card-body flex flex-col items-center">
           <h2 className="card-title my-8">Masuk</h2>
           <input type="text" placeholder="Nama Pengguna" className="input input-bordered w-full max-w-xs mb-5" />
-          <div className="form-control w-full max-w-xs mb-5">
+          <div className="form-control w-full max-w-[320px] mb-5">
             <input type="password" placeholder="Kata Sandi" className="input input-bordered w-full max-w-xs" />
             <label className="label">
               <span className="label-text-alt underline hover:cursor-pointer">Lupa Password?</span>
             </label>
           </div>
           <div className='flex flex-col items-center'>
-            <button className="btn rounded-full border-[0.3px] border-slate-400 bg-transparent w-32 h-8 mb-5 hover:bg-white">Masuk</button>
+            <button className="btn rounded-full border-[0.3px] border-slate-400 bg-transparent w-[70%] h-8 mb-5 hover:bg-white">Masuk</button>
             <p>Tidak punya akun? <Link href={'/register'} className="font-medium text-base">
               <span className='font-bold underline hover:cursor-pointer'>Daftar</span>
             </Link></p>
--- src/components/teams/Teams.js
+++ src/components/teams/Teams.js
@@ -7,12 +7,13 @@ export default function Teams() {
         <div className='flex flex-col items-center my-10'>
             <h1 className='font-extrabold text-3xl text-[#E9F5F5]'>Tim Kita</h1>
         </div>
-        <div className='grid grid-cols-2 flex-wrap gap-7 max-w-[50vw] justify-center items-center'>
-            <TeamsCard name={"Aryo Bimo Wicaksono"} nim={"21.11.4311"}/>
-            <TeamsCard name={"Ameilia Nurcahyandari S."} nim={"21.11.4314"}/>
-            <TeamsCard name={"Annisa Damayanti"} nim={"21.11.4260"}/>
-            <TeamsCard name={"Mauhiba Salmaa G."} nim={"21.11.4264"}/>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-7 max-w-screen-lg mx-auto justify-center items-center'>
+            <TeamsCard name={"Aryo Bimo Wicaksono"} nim={"21.11.4311"} />
+            <TeamsCard name={"Ameilia Nurcahyandari S."} nim={"21.11.4314"} />
+            <TeamsCard name={"Annisa Damayanti"} nim={"21.11.4260"} />
+            <TeamsCard name={"Mauhiba Salmaa G."} nim={"21.11.4264"} />
         </div>
+
     </div>
   )
 }
